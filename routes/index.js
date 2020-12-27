c828f243f050ea8a203cae5b8d3ce2c679433a8d
MinjaeLee <sochubert@gmail.com>
2020-12-27 14:11:09 +0900

    #2,#3

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,50 @@
 var express = require('express');
 var router = express.Router();
 
+var mysql = require('mysql');
+var db_config = {
+  host : 'us-cdbr-east-02.cleardb.com',
+  user : 'b503052a923146',
+  password : '3e78d93a',
+  database : 'heroku_c01164af4998693'
+};
+
+var connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'index' });
+});
+
+router.get('/question', function(req, res) {
+  res.render('question', { title: 'question'});
+});
+
+router.get('/request', function(req, res) {
+  res.render('request', { title: 'request'});
 });
 
 module.exports = router;
