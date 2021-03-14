fe7824d41eac377a721629c2d8db02082fe54cee
MinjaeLee <sochubert@gmail.com>
2021-03-14 17:02:23 +0900

    fnq

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'index' });
+});
+
+router.get('/fnq', function(req, res, next) {
+  res.render('fnq', { title: 'fnq' });
+});
+
+router.get('/event', function(req, res, next) {
+  res.render('event', { title: 'event' });
 });
 
 module.exports = router;
